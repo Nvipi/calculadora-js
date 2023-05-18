6beda57c754942f897f4601c6b8b4d7918801a0a
josue <josue.parada.tapia@gmail.com>
2023-05-18 03:16:00 -0400

    Fix: Corregir calculos

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,5 @@
 // Opciones validas de la calculadora
-const opciones = ["(",")","C","9","8","7","6","5","4","3","2","1","0","/","*","+","-",".","b","=","Backspace","Enter"];
+const opciones = ["(",")","C","9","8","7","6","5","4","3","2","1","0","/","*","+","-",".","b","=","Backspace","Enter","Delete"];
 
 // Obtenemos el elemento que mostrara el calculo/resultado
 const pantalla = document.getElementById("resultado");
@@ -13,6 +13,22 @@ function realizarOperacion(fn) {
     return new Function('return ' + fn)();
 }
 
+// Creamos un campo "invisible" con el resultado, debido a que la pantalla no logra mostrar correctamente el valor tan largo
+function copiarAlPortapapeles(valor) {
+    // creamos campo
+    const aux = document.createElement("input");
+    // le asignamos el valor
+    aux.setAttribute("value", valor);
+    // Agregamos el campo al body
+    document.body.appendChild(aux);
+    // Seleccionamos el campo y su valor
+    aux.select();
+    // Copiamos el valor
+    document.execCommand("copy");
+    // Eliminamos el campo
+    document.body.removeChild(aux);
+}
+
 // Funcion a realizar cuando se clickea un boton
 const digitar = function (e) {
     // Comprobar que lo digitado (click/teclado) esta dentro de los valores permitidos
@@ -20,8 +36,8 @@ const digitar = function (e) {
         // Valor ingresado
         const valor = (this.value) ? this.value : e.key;
 
-        // Si lo ultimo ocurrido es un error
-        if(pantalla.innerText == "Error") pantalla.innerText = "";
+        // Si lo ultimo ocurrido es un error o se copio
+        if(pantalla.innerText == "Error" || pantalla.innerText == "Resultado en portapapeles") pantalla.innerText = "";
 
         // Obtenemos el valor actual en pantalla
         const resultado = pantalla.innerText;
@@ -32,16 +48,31 @@ const digitar = function (e) {
         if(valor == "C" || valor == "Delete") return pantalla.innerText = "";
 
         // Borrar un digito/valor/etc
-        if(valor == "b" || valor == "Backspace" && resultado.length > 0) return pantalla.innerText = resultado.slice(0,(resultado.length-1));
+        if(valor == "b" || valor == "Backspace") return pantalla.innerText = resultado.slice(0,(resultado.length-1));
 
         // Agregar un digito/valor/etc con limitacion de 16 caracteres
-        if(valor != "b" && valor != "Backspace" && valor != "=" && valor != "Enter" && resultado.length < 16) return pantalla.innerText = resultado + valor;
+        if(valor != "=" && valor != "Enter" && resultado.length < 12) return pantalla.innerText = resultado + valor;
 
-        // Obtener el resultado y actuar en caso de la operacion no este estructurada correctamente
+        // Obtener el resultado y actuar en caso de que la operacion no este estructurada correctamente
         try{
-            if(valor == "=" || valor == "Enter") return pantalla.innerText = realizarOperacion(resultado);
+            if(valor == "=" || valor == "Enter"){
+                // Guardamos el resultado de la operacion luego de llamar a la funcion
+                const resul = realizarOperacion(resultado);
+
+                // Si el resultado supera el tamaño permitido por la pantalla, agregamos el valor a el portapapeles
+                if(resul.toString().length > 11){
+                    // Copiar el resultado
+                    copiarAlPortapapeles(resul);
+                    // Imprimir la accion de copiar el resultado en su portapapeles
+                    return pantalla.innerText = "Resultado en portapapeles";
+                }else{
+                    // Retornar el valor en pantalla
+                    return pantalla.innerText = (resul)?resul:"Error";
+                };
+            }
 
         }catch{
+            // Mostrar error en pantalla en caso que la operacion no se pueda efectuar
             pantalla.innerText = "Error";
             
         }
